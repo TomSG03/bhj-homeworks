7b59b5eb48bcd4f634a795699b4563516d11ed2a
Tomilov Sergey <tomexpert@yandex.ru>
2021-07-30 21:20:44 +0800

    2.1 DOM v2

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,22 +1,32 @@
-class Spin {
-  constructor(spinner) {
-    this.rotor = Array.from(spinner.querySelectorAll('.rotator__case'));
+class Rotator {
+  constructor(rotor) {
+    this.rotor = rotor;
     this.next();
-    this.timeOut = 1;
+    this.fl = true;
   }
 
   next() {
     setTimeout(function (ths) {
-      let index = ths.rotor.findIndex(element => element.classList.contains('rotator__case_active') === true);
-      ths.rotor[index].classList.remove('rotator__case_active');
-      index = (index === ths.rotor.length - 1) ? index = 0 : index += 1;
-      ths.rotor[index].classList.add('rotator__case_active');
-      ths.rotor[index].style.color = ths.rotor[index].dataset.color;
-      ths.timeOut = ths.rotor[index].dataset.speed;
-      setTimeout(ths.next(), ths.timeOut);
-    }, this.timeOut, this);
-  };
+      if (ths.fl === false) {
+        ths.rotor.classList.toggle('rotator__case_active');
+        ths.rotor = ths.rotor.parentElement.firstElementChild;
+        ths.rotor.style.color = ths.rotor.dataset.color;
+        ths.rotor.classList.toggle('rotator__case_active');
+        ths.fl = true;
+      } else {
+        ths.rotor.classList.toggle('rotator__case_active');
+        ths.rotor = ths.rotor.nextElementSibling;
+        ths.rotor.style.color = ths.rotor.dataset.color;
+        ths.rotor.classList.toggle('rotator__case_active');
+        
+        if (ths.rotor.nextElementSibling === null) {
+          ths.fl = false;
+        }
+      }
+      setTimeout(ths.next(), ths.rotor.dataset.speed);
+    }, this.rotor.dataset.speed, this);
+  }
 }
 
-Array.from(document.querySelectorAll('.rotator')).forEach(item => new Spin(item));;
+Array.from(document.querySelectorAll('.rotator')).forEach(item => new Rotator(item.firstElementChild));
 
