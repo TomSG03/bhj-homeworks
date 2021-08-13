80666a3f39e588cfe76dba2ecc10091a65811272
Tomilov Sergey <tomexpert@yandex.ru>
2021-08-13 20:57:55 +0800

    3.1 Хранение состояния на клиенте v1.1

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,8 +1,15 @@
-if (localStorage.modalClosed === undefined) {
+function chekSubscrib() {
+  const cookies = document.cookie.split('; ')
+  const result = cookies.find(c => c.startsWith('modal'))
+
+  return result ? decodeURIComponent(result.substr(6)) : null
+}
+
+if (chekSubscrib() != 'closed') {
   document.getElementById('subscribe-modal').classList.add('modal_active');
 }
 
 document.querySelector('.modal__close').addEventListener('click', () => {
-  localStorage.modalClosed = true;
-  document.getElementById('subscribe-modal').classList.remove('modal_active');
+   document.cookie = 'modal=closed'
+   document.getElementById('subscribe-modal').classList.remove('modal_active');
 })
