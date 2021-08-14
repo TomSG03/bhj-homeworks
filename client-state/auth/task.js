1acfe68d9379f995f541965b985272ce7c0b3bfc
Tomilov Sergey <tomexpert@yandex.ru>
2021-08-14 09:29:53 +0800

    3.1 Хранение состояния на клиенте v1.2

---
+++ client-state/auth/task.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('signin__form');
+const welcome = document.getElementById('welcome');
+const userId = document.getElementById('user_id');
+
+document.getElementById('signin__btn').addEventListener('click', (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
+  xhr.send(formData);
+
+  xhr.onload = () => {
+    const rezult = JSON.parse(xhr.responseText);
+
+    if (rezult.success) {
+      form.closest('.signin').classList.remove('signin_active');
+      welcome.classList.add('welcome_active');
+      userId.textContent = rezult.user_id;
+      localStorage.user_id = rezult.user_id;
+    }
+    else {
+      alert('Неверный логин/пароль');
+    }
+    [...form.querySelectorAll('.control')].forEach(item => item.value = '');
+  }
+})
+
+document.getElementById('signout__btn').addEventListener('click', (event) => {
+  form.closest('.signin').classList.add('signin_active');
+  welcome.classList.remove('welcome_active');
+  localStorage.removeItem('user_id');
+
+})
+
+window.onload = () => {
+  if (localStorage.user_id != undefined) {
+    form.closest('.signin').classList.remove('signin_active');
+    welcome.classList.add('welcome_active');
+    userId.textContent = localStorage.user_id;
+  }
+}
